3287907c9c1696948d53d5d495f145f98997aaf0
Dflorez015 <duvanflorez013@gmail.com>
2021-05-22 18:38:27 -0500

    bootstratp install

--- projectWeb2/resources/views/welcome.blade.php
+++ projectWeb2/resources/views/welcome.blade.php
@@ -7,10 +7,27 @@
 
     <title>Laravel</title>
 
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    </link>
+
 </head>
 
-<body class="container">
-    Hola
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+            <a class="navbar-brand">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active" href="#">Features</a>
+                    <a class="nav-item nav-link active" href="#">Pricing</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <script src="js/app.js"></script>
 </body>
 
 </html>
